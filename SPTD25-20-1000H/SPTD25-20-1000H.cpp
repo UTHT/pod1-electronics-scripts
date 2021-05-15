bb4d48f899da6a18af2812b97e979b76f49fe951
Urmil Modi <urmilnmodi@gmail.com>
2021-05-15 12:38:00 -0400

    renamed mapping function and added a todo

--- SPTD25-20-1000H/SPTD25-20-1000H.cpp
+++ SPTD25-20-1000H/SPTD25-20-1000H.cpp
@@ -16,16 +16,16 @@ errorlevel_t SPTD25_20_1000H::init(){
 // Currently sensorValue at 0 psi is 176 - 177
 errorlevel_t SPTD25_20_1000H::read(t_datum* data, uint8_t numdata){
     // NOTE: Convention - check that numdata given matches expected
-    if(numdata != 2){
+    if(numdata != 2) { //TODO: globally declare the array size instead of using the int value
       return ERR_FAIL;
     }
     int val = analogRead(pin);
     data[0].data = (float)val;
-    data[1].data = (float)sptd25_map(val, 176.0, 1023.0, 0, 200.0);
+    data[1].data = (float)sptd25_20_1000h_map(val, 176.0, 1023.0, 0, 200.0);
     // TODO: other error conditions?
     return ERR_NONE;
 }
 
-double sptd25_map(const int x, const double in_min, const double in_max, const double out_min, const double out_max) {
+double sptd25_20_1000h_map(const int x, const double in_min, const double in_max, const double out_min, const double out_max) {
   return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
