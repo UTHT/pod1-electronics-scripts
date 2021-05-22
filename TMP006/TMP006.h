59917904b1a5c1077f16973aab5461ecc1a1e27b
Jayden Lefebvre <jayden.lefebvre55@gmail.com>
2021-05-22 11:32:31 -0400

    Implementation

--- TMP006/TMP006.h
+++ TMP006/TMP006.h
@@ -8,7 +8,7 @@
 
 class TMP006 : public Sensor {
     public:
-        TMP006(uint8_t addr, arduino_t arduino);
+        TMP006(uint8_t addr = TMP006_I2CADDR, arduino_t arduino);
     private:
         Adafruit_TMP006 tmp006;
         errorlevel_t read(t_datum* data, uint8_t numdata) override;
