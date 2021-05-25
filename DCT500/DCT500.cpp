d5bcdd1a4876f0f7d57404cc3b0656a250e2f38f
Alex <alexzengxi@gmail.com>
2021-05-25 15:25:56 -0400

    fixed bugs

--- DCT500/DCT500.cpp
+++ DCT500/DCT500.cpp
@@ -23,16 +23,15 @@ errorlevel_t DCT500::read(t_datum* data, uint8_t numdata){
       return ERR_FAIL;
     }
 
-    const double resistor = 100     // ohm, depends on the resistor that we are using 
-    double ouput_voltage = analogRead(pin)*5/1023.0/100;     // in V, (arduino has a max of 5V, the sensor outputs 10V)
-    
-    // Calibrate from analogRead to 10V
-    double ouput_voltage = dct500_map(ouput_voltage, 0, 1023, 0, 10)
+    const double resistor = 200     // ohm (we are using TWO 100 ohm ressitor for a voltage divider) 
+    double ouput_voltage = analogRead(pin);     // in V
 
+    // Calibrate from analogRead to 10V (AnalogRead has a max of 5V, the sensor outputs 10V, we are using a voltage divider )
+    double ouput_voltage = dct500_map(ouput_voltage, 0, 1023, 0, 10)
     double read_current = ouput_voltage / resistor
 
     // Calibrate from 0 A to 500 A
-    double calculated_current = dct500_map(read_current, 4, 20, 0, 500)     // in A
+    double calculated_current = dct500_map(read_current, 4, 20, 0, 500)
 
     data[0].data = (float)calculated_current;
     // TODO: other error conditions?
