d1fa0eba47069d1209254c57b898410a3fda19e5
Alex <alexzengxi@gmail.com>
2021-05-27 12:49:57 -0400

    Merge branch 'sensor-dct500' of https://github.com/UTHT/electronics-scripts into sensor-dct500

--- OPT2002/src/OPT2002.cpp
+++ OPT2002/src/OPT2002.cpp
@@ -14,7 +14,11 @@ errorlevel_t OPT2002::init(){
     return ERR_NONE;
 }
 
-// TODO Calibrate distance reading
+// Maping function for DCT500
+double opt2002_map(const int x, const double in_min, const double in_max, const double out_min, const double out_max){
+    return (x - in_min) / (in_max - in_min) * (out_max - out_min) + out_min;
+}
+
 errorlevel_t OPT2002::read(t_datum* data, uint8_t numdata){
     // NOTE: Convention - check that numdata given matches expected
     if(numdata != 1){ //TODO: globally declare the array size instead of using the int value
@@ -29,8 +33,18 @@ errorlevel_t OPT2002::read(t_datum* data, uint8_t numdata){
     // if(error.length() > 0){
     //     return ERR_WARN;
     // }
+
+    // Set its analog output as voltage output(10V)
+    const double resistor = 300;     // ohm (we are using THREE 100 ohm ressitor for a voltage divider) 
+    // For Arduino Due voltage divider using 3 100 ohm resistors, 10V input read as 3.3V at the Arduino
+    // For Arduino Mega voltage divider using 2 100 ohm resistors, 10V input read as 5.0V at the Arduino
     
-    double distance = 30 + 5 * (analogRead(inpin) * 3.3 * 2);	
+    // Calibrate from analogRead to 10V (undoing the voltage divider)
+    double voltage = dct500_map(analogRead(pin), 0, 1023, 0, 10);
+    double current = voltage / resistor;
+
+    // Calibrate from 30mm to 80mm
+    double distance = dct500_map(current, 4, 20, 30, 80);
 
     data[0].data = (float)distance;
     // TODO: other error conditions?
