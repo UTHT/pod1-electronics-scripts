3c67435126921a7e57653a95d00563ce21c279eb
pietrea2 <60241038+pietrea2@users.noreply.github.com>
2021-08-14 10:56:14 -0400

    Renamed function to create_command_message

--- Serial-CAN-Bus/PM100_CAN_BUS.h
+++ Serial-CAN-Bus/PM100_CAN_BUS.h
@@ -2,19 +2,19 @@
 #define PM100_CAN_BUS_H
 
 //types of CAN messages
-typedef enum message_type {
+typedef enum m_type {
   BROADCAST_M,
   COMMAND_TOURQUE_M,
   COMMAND_SPEED_M
 } message_type;
 
 //command message variables for inverter
-typedef enum inverter_direction {
+typedef enum i_direction {
   REVERSE, //clock-wise
   FORWARD  //counter-clock-wise
 } inverter_direction;
 
-typedef enum inverter_enable {
+typedef enum i_enable {
   ENABLE,
   DISABLE
 } inverter_enable;
@@ -27,11 +27,11 @@ class command_message{
   
     message_type command_message;
     
-    int send_command_message( message_type command_message, 
-                              float torque_val, //Value in N.m
-                              float speed_val,  //RPM
-                              inverter_direction direction_command, 
-                              inverter_enable enable_command );
+    int* create_command_message( message_type command_message, 
+                                 float torque_val, //Value in N.m
+                                 float speed_val,  //RPM
+                                 inverter_direction direction_command, 
+                                 inverter_enable enable_command );
 
 
                               
