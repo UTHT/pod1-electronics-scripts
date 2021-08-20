ded21a7fd0c68fb39a640beab8675488b76c2b65
Alex <alexzengxi@gmail.com>
2021-08-20 19:07:29 -0400

    Fixing bugs

--- DCT500/src/DCT500.cpp
+++ DCT500/src/DCT500.cpp
@@ -14,7 +14,7 @@ errorlevel_t DCT500::init(){
 
 // Maping function for DCT500
 double dct500_map(const int x, const double in_min, const double in_max, const double out_min, const double out_max){
-    return (x - in_min) / (in_max - in_min) * (out_max - out_min) + out_min;
+    return out_min + (((x - in_min) / (in_max - in_min)) * (out_max - out_min));
 }
 
 errorlevel_t DCT500::read(t_datum* data, uint8_t numdata){
@@ -23,19 +23,20 @@ errorlevel_t DCT500::read(t_datum* data, uint8_t numdata){
       return ERR_FAIL;
     }
 
-    const double resistor = 300;
+    const double resistor = 200;
     // For Arduino Due use a 300 ohm resistor, 10V input read as 3.3V at the Arduino
     // For Arduino Mega use a 200 ohm resistors, 10V input read as 5.0V at the Arduino
     
     // Calibrate from analogRead to 10V (undoing the resistor)
-    double output_voltage = dct500_map(analogRead(pin), 0, 1023, 0, 10);
-    double read_current = output_voltage / resistor;
-    double calculated_current = 0;
+    double calculated_current = dct500_map(analogRead(pin), 160, 819, 0, 500);
+    // double read_current = output_voltage / resistor;
+    // double calculated_current = 0;
 
+    /*
     if(read_current != 0){
         // Calibrate from 0 A to 500 A
         calculated_current = dct500_map(read_current, 4, 20, 0, 500);
-    }
+    }*/
     
     data[0].data = (float)calculated_current;
     // TODO: other error conditions?
