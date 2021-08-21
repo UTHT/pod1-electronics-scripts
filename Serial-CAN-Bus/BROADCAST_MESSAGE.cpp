9b942ea19abb6e6dbb5da51beffde8b1cff98b26
Angus Wu <wuangusjh@gmail.com>
2021-08-21 03:08:37 -0400

    Added disable broadcast messages function

--- Serial-CAN-Bus/BROADCAST_MESSAGE.cpp
+++ Serial-CAN-Bus/BROADCAST_MESSAGE.cpp
@@ -1,19 +1,19 @@
 #include <iostream>
 #include "BROADCAST_MESSAGE.h"
 
-using namesapce std;
+using namespace std;
 
-// fyi unsigned char message_arr[9] = {CAN ID, data byte 1, data byte 2, ...}
+// fyi unsigned char CAN_mssg[9] = {CAN ID, data byte 1, data byte 2, ...}
 
-void print_vsm_state(unsigned char message_arr[9])
+void print_vsm_state(unsigned char CAN_mssg[9]) // TODO restructure like Fault Code File
 {
-    if (message_arr[0] != 0xAA)
+    if (CAN_mssg[0] != 0xAA)
     {
         cout << "Message CAN ID is not 0xAA" << endl;
         return;
     }
 
-    switch (message_arr[2]) // message_arr[1] should be 0
+    switch (CAN_mssg[2]) // CAN_mssg[1] should be 0
     {
     case 0:
         cout << "VSM State: VSM Start State";
@@ -50,15 +50,15 @@ void print_vsm_state(unsigned char message_arr[9])
     }
 }
 
-void print_inverter_state(unsigned char message_arr[9])
+void print_inverter_state(unsigned char CAN_mssg[9]) // TODO restructure like Fault Code File
 {
-    if (message_arr[0] != 0xAA)
+    if (CAN_mssg[0] != 0xAA)
     {
         cout << "Message CAN ID is not 0xAA" << endl;
         return;
     }
 
-    switch (message_arr[3])
+    switch (CAN_mssg[3])
     {
     case 0:
         cout << "Inverter State: Power on State";
@@ -84,4 +84,20 @@ void print_inverter_state(unsigned char message_arr[9])
     default:
         cout << "Invalid Inverter State";
     }
+}
+
+// Disabling a specific broadcast message
+// TODO ability to disable multiple broadcast messages at a time (Vector?, bitwise and param?)
+void disable_broadcast_message(unsigned char (&CAN_mssg)[9], can_active_messages_lo_word content)
+{
+    if (CAN_mssg[1] != 148) // Data Byte 0
+    {
+        cout << "Message Parameter Address is not 148" << endl;
+        return;
+    }
+
+    if (content < 8)
+        CAN_mssg[5] &= ~(1U << content); // Clearing the corresponding bit in Data Byte 4
+    else
+        CAN_mssg[6] &= ~(1U << (content - 8)); // Clearing the corresponding bit in Data Byte 5
 }
--- Serial-CAN-Bus/BROADCAST_MESSAGE.h
+++ Serial-CAN-Bus/BROADCAST_MESSAGE.h
@@ -30,12 +30,53 @@ typedef enum inverter_state
     IDLE_STOP
 } inverter_state;
 
+// Reference Param Address 148
+typedef enum can_active_messages_lo_word
+{
+    // Data Byte 4
+    TEMPERATURE_1,
+    TEMPERATURE_2,
+    TEMPERATURE_3,
+    ANALOG_INPUT_VOLTAGES,
+    DIGITAL_INPUT_STATUS,
+    MOTOR_POSITION_INFORMATION,
+    CURRENT_INFORMATION,
+    VOLTAGE_INFORMATION,
+    // Data Byte 5
+    FLUX_INFORMATION,
+    INTERNAL_VOLTAGES,
+    INTERNAL_STATES,
+    FAULT_CODES,
+    TORQUE_AND_TIMER_INFORMATION,
+    MODULATION_INDEX_AND_FLUX_WEAKENING_OUTPUT_INFORMATION,
+    FIRMWARE_INFORMATION,
+    DIAG_DATA
+} can_active_messages_lo_word;
+
+// Reference Param Address 148, should always be enabled (default)
+typedef enum can_active_messages_hi_word
+{
+    // Data Byte 6
+    SLAVE_MODE_COMMAND_MESSAGE = 6,
+    BMS_COMMAND_MESSAGE,
+    // Data Byte 7
+    OBD2_GENERAL_QUERY,
+    OBD2_SPECIFIC_QUERY,
+    OBD2_RESPONSE,
+    U2C_TX_MESSAGE,
+    U2C_RX_MESSAGE,
+    PARAMETER_RESPONSE_MESAGE,
+    PARAMETER_COMMAND_MESSAGE,
+    CAN_COMMAND_MESSAGE
+} can_active_messages_hi_word;
+
 class broadcast_message
 {
 public:
     message_type broadcast_message;
     void print_vsm_state(int message_arr[9]);
     void print_inverter_state(int message_arr[9]);
+    void disable_broadcast_message(unsigned char (&CAN_mssg)[9], can_active_messages_lo_word content);
 }
 
 #endif
